434a73de699b3f8eb9dea4e18d00edca1b981876
Ange A. Buhendwa <mickaelbhd@gmail.com>
2026-02-21 01:52:01 +0200

    chore: initialize project with all the services

---
+++ api-gateway/src/test/java/com/amalitech/fooddelivery/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.fooddelivery.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ customer-service/src/test/java/com/amalitech/fooddelivery/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.fooddelivery.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
